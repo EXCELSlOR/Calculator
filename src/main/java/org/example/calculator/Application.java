99030890a232e7920469abfa98eb1158abab8343
excelsior <excelsior@mail.ru>
2023-12-22 06:56:40 +0300

    создание разметки приложения

--- src/main/java/org/example/calculator/Application.java
+++ src/main/java/org/example/calculator/Application.java
@@ -10,8 +10,9 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Калькулятор");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
